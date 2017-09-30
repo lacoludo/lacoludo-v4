8c528fdd97860cea25d7ad61a2ea6229020ee752
LACOUTURE Ludovic <email@lacoludo.com>
2017-09-30 23:33:24 +0200

    js added

---
+++ js/custom-index.js
@@ -0,0 +1,269 @@
+(function($) {
+  "use strict";
+
+  function Selector_Cache() {
+    var collection = {};
+
+    function get_from_cache(selector) {
+      if (undefined === collection[selector]) {
+        collection[selector] = $(selector);
+      }
+
+      return collection[selector];
+    }
+
+    return {get: get_from_cache};
+  }
+
+  var selectors = new Selector_Cache();
+
+  //Preloader
+
+  Royal_Preloader.config({mode: 'progress', background: '#161616', showProgress: true, showPercentage: false});
+
+  /* Scroll Animation */
+
+  window.scrollReveal = new scrollReveal();
+
+  //Home text fade on scroll
+
+  $(window).scroll(function() {
+    var $Fade = $('.fade-elements');
+    //Get scroll position of window
+    var windowScroll = $(this).scrollTop();
+    //Slow scroll and fade it out
+    $Fade.css({
+      'margin-top': -(windowScroll / 0) + "px",
+      'opacity': 1 - (windowScroll / 300)
+    });
+  });
+
+  $(document).ready(function() {
+
+    //Scroll back to top
+
+    var offset = 450;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery('.scroll-to-top').fadeIn(duration);
+      } else {
+        jQuery('.scroll-to-top').fadeOut(duration);
+      }
+    });
+
+    jQuery('.scroll-to-top').on('click', function(event) {
+      event.preventDefault();
+      jQuery('html, body').animate({
+        scrollTop: 0
+      }, duration);
+      return false;
+    })
+
+    //Tooltip
+
+    $(".tipped").tipper();
+
+    //Home Carousel
+
+    var sync1 = $("#sync1");
+    var sync2 = $("#sync2");
+
+    sync1.owlCarousel({
+      singleItem: true,
+      autoPlay: 5000,
+      transitionStyle: "fade",
+      autoHeight: false,
+      slideSpeed: 200,
+      navigation: false,
+      pagination: false,
+      afterAction: syncPosition,
+      responsiveRefreshRate: 200
+    });
+
+    sync2.owlCarousel({
+      items: 3,
+      itemsDesktop: [
+        1199, 3
+      ],
+      itemsDesktopSmall: [
+        979, 3
+      ],
+      itemsTablet: [
+        768, 3
+      ],
+      itemsMobile: [
+        479, 3
+      ],
+      pagination: false,
+      responsiveRefreshRate: 100,
+      afterInit: function(el) {
+        el.find(".owl-item").eq(0).addClass("synced");
+      }
+    });
+
+    function syncPosition(el) {
+      var current = this.currentItem;
+      $("#sync2").find(".owl-item").removeClass("synced").eq(current).addClass("synced")
+      if ($("#sync2").data("owlCarousel") !== undefined) {
+        center(current)
+      }
+    }
+
+    $("#sync2").on("click", ".owl-item", function(e) {
+      e.preventDefault();
+      var number = $(this).data("owlItem");
+      sync1.trigger("owl.goTo", number);
+    });
+
+    function center(number) {
+      var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+      var num = number;
+      var found = false;
+      for (var i in sync2visible) {
+        if (num === sync2visible[i]) {
+          var found = true;
+        }
+      }
+
+      if (found === false) {
+        if (num > sync2visible[sync2visible.length - 1]) {
+          sync2.trigger("owl.goTo", num - sync2visible.length + 2)
+        } else {
+          if (num - 1 === -1) {
+            num = 0;
+          }
+          sync2.trigger("owl.goTo", num);
+        }
+      } else if (num === sync2visible[sync2visible.length - 1]) {
+        sync2.trigger("owl.goTo", sync2visible[1])
+      } else if (num === sync2visible[0]) {
+        sync2.trigger("owl.goTo", num - 1)
+      }
+
+    }
+
+    //Navigation
+
+    //open navigation clicking the menu icon
+    $('.cd-nav-trigger').on('click', function(event) {
+      event.preventDefault();
+      toggleNav(true);
+    });
+    //close the navigation
+    $('.cd-close-nav, .cd-overlay').on('click', function(event) {
+      event.preventDefault();
+      toggleNav(false);
+    });
+
+    function toggleNav(bool) {
+      $('.cd-nav-container, .cd-overlay').toggleClass('is-visible', bool);
+      $('main').toggleClass('scale-down', bool);
+    }
+    $('.cd-nav li').on('click', function(event) {
+      //close navigation
+      toggleNav(false);
+    });
+
+    /* Portfolio Sorting */
+
+    (function($) {
+
+      var container = $('#projects-grid');
+
+      function getNumbColumns() {
+        var winWidth = $(window).width(),
+          columnNumb = 1;
+
+        if (winWidth > 1500) {
+          columnNumb = 4;
+        } else if (winWidth > 1200) {
+          columnNumb = 3;
+        } else if (winWidth > 900) {
+          columnNumb = 2;
+        } else if (winWidth > 600) {
+          columnNumb = 2;
+        } else if (winWidth > 300) {
+          columnNumb = 1;
+        }
+
+        return columnNumb;
+      }
+
+      function setColumnWidth() {
+        var winWidth = $(window).width(),
+          columnNumb = getNumbColumns(),
+          postWidth = Math.floor(winWidth / columnNumb);
+
+      }
+
+      $('#portfolio-filter #filter a').click(function() {
+        var selector = $(this).attr('data-filter');
+
+        $(this).parent().parent().find('a').removeClass('current');
+        $(this).addClass('current');
+
+        container.isotope({filter: selector});
+
+        setTimeout(function() {
+          reArrangeProjects();
+        }, 300);
+
+        return false;
+      });
+
+      function reArrangeProjects() {
+        setColumnWidth();
+        container.isotope('reLayout');
+      }
+
+      container.imagesLoaded(function() {
+        setColumnWidth();
+
+        container.isotope({itemSelector: '.portfolio-box-1', layoutMode: 'masonry', resizable: false});
+      });
+
+      $(window).on('debouncedresize', function() {
+        reArrangeProjects();
+
+      });
+
+    })(jQuery);
+
+  });
+
+  /* DebouncedResize Function */
+  (function($) {
+    var $event = $.event,
+      $special,
+      resizeTimeout;
+
+    $special = $event.special.debouncedresize = {
+      setup: function() {
+        $(this).on('resize', $special.handler);
+      },
+      teardown: function() {
+        $(this).off('resize', $special.handler);
+      },
+      handler: function(event, execAsap) {
+        var context = this,
+          args = arguments,
+          dispatch = function() {
+            event.type = 'debouncedresize';
+
+            $event.dispatch.apply(context, args);
+          };
+
+        if (resizeTimeout) {
+          clearTimeout(resizeTimeout);
+        }
+
+        execAsap
+          ? dispatch()
+          : resizeTimeout = setTimeout(dispatch, $special.threshold);
+      },
+      threshold: 150
+    };
+  })(jQuery);
+
+})(jQuery);
