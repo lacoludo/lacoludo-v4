201c50538eb0caaecf8a02d3a8139c059e424a17
lacoludo <email@lacoludo.com>
2019-06-20 21:30:13 +0200

    showcase

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,6 @@
-	
-jQuery(document).ready(function ($) { // wait until the document is ready
-	$('#send').on('click', function(){ // when the button is clicked the code executes
-		$('.error').fadeOut('slow'); // reset the error messages (hides them)
-
-		var error = false; // we will set this true if the form isn't valid
-
-		var name = $('input#name').val(); // get the value of the input field
-		if(name == "" || name == " ") {
-			$('#err-name').fadeIn('slow'); // show the error message
-			error = true; // change the error state to true
-		}
-
-		var email_compare = /^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/; // Syntax to compare against input
-		var email = $('input#email').val(); // get the value of the input field
-		if (email == "" || email == " ") { // check if the field is empty
-			$('#err-email').fadeIn('slow'); // error - empty
-			error = true;
-		}else if (!email_compare.test(email)) { // if it's not empty check the format against our email_compare variable
-			$('#err-emailvld').fadeIn('slow'); // error - not right format
-			error = true;
-		}
-
-		if(error == true) {
-			$('#err-form').slideDown('slow');
-			return false;
-		}
-
-		var data_string = $('#ajax-form').serialize(); // Collect data from form
-
-		$.ajax({
-			type: "POST",
-			url: $('#ajax-form').attr('action'),
-			data: data_string,
-			timeout: 6000,
-			error: function(request,error) {
-				if (error == "timeout") {
-					$('#err-timedout').slideDown('slow');
-				}
-				else {
-					$('#err-state').slideDown('slow');
-					$("#err-state").html('An error occurred: ' + error + '');
-				}
-			},
-			success: function() {
-				$('#ajax-form').slideUp('slow');
-				$('#ajaxsuccess').slideDown('slow');
-			}
-		});
-
-		return false; // stops user browser being directed to the php file
-	}); // end click function
-});
+jQuery(document).ready(function(e){e("#send").on("click",function(){e(".error").fadeOut("slow")
+var r=!1,a=e("input#name").val();(""==a||" "==a)&&(e("#err-name").fadeIn("slow"),r=!0)
+var o=/^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/,t=e("input#email").val()
+if(""==t||" "==t?(e("#err-email").fadeIn("slow"),r=!0):o.test(t)||(e("#err-emailvld").fadeIn("slow"),r=!0),1==r)return e("#err-form").slideDown("slow"),!1
+var n=e("#ajax-form").serialize()
+return e.ajax({type:"POST",url:e("#ajax-form").attr("action"),data:n,timeout:6e3,error:function(r,a){"timeout"==a?e("#err-timedout").slideDown("slow"):(e("#err-state").slideDown("slow"),e("#err-state").html("An error occurred: "+a))},success:function(){e("#ajax-form").slideUp("slow"),e("#ajaxsuccess").slideDown("slow")}}),!1})})
